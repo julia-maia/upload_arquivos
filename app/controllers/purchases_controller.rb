56dcdd153aaa135932362a8211d0446b8dfee46c
julia-maia <brambatti1212@gmail.com>
2025-05-13 09:52:15 -0300

    Adicionado Páginação e Funcionalidade de Criar Nova Compra

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,28 @@
 class PurchasesController < ApplicationController
   def index
+    @per_page = 10  # Itens por página
+    @current_page = (params[:page] || 1).to_i  # Página atual
+
     @purchases = Purchase.includes(:purchaser, item: :merchant)
+                        .order(created_at: :desc)
+                        .offset((@current_page - 1) * @per_page)
+                        .limit(@per_page)
+
+    total_purchases = Purchase.count
+    @total_pages = (total_purchases.to_f / @per_page).ceil
+  end
+
+  def new
+    @purchase = Purchase.new
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.save
+      redirect_to @purchase, notice: "Compra criada com sucesso."
+    else
+      render :new
+    end
   end
 
   def destroy
